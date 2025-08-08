e70fe649f338bcc540cf9c11d5454b1cc18d757d
Raony Fernandes dos Reis <75029692+raonygamer@users.noreply.github.com>
2025-08-08 13:54:43 -0300

    Add logging, CLI options, and inspect command
    Introduced a Common.Log utility for colored console output. Refactored Program to use System.CommandLine, added version/author info, and implemented an --inspect option for .pak file validation. Added launchSettings.json for debugging and updated project dependencies.

---
+++ SCPakTool/Common/Log.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common {
+    public static class Log {
+        public static ConsoleColor Color {
+            get => Console.ForegroundColor;
+            set => Console.ForegroundColor = value;
+        }
+
+        public static void Write(object? obj = null, ConsoleColor color = ConsoleColor.White) {
+            var oldColor = Console.ForegroundColor;
+            Color = color;
+            Console.WriteLine(obj);
+            Color = oldColor;
+        }
+
+        public static void Trace(object? obj = null) => Write($"[SCPakTool {DateTime.Now:HH:mm:ss}] {obj}", ConsoleColor.White);
+        public static void Warn(object? obj = null) => Write($"[SCPakTool {DateTime.Now:HH:mm:ss}] {obj}", ConsoleColor.Yellow);
+        public static void Error(object? obj = null) => Write($"[SCPakTool {DateTime.Now:HH:mm:ss}] {obj}", ConsoleColor.Red);
+    }
+}
--- SCPakTool/Program.cs
+++ SCPakTool/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace SCPakTool
+﻿using Common;
+using System.CommandLine;
+using System.IO;
+using System.Text;
+
+namespace SCPakTool
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static Program? _instance;
+        public static Program Instance => _instance ??= new Program();
+        static int Main(string[] args) => Instance.Start(args).GetAwaiter().GetResult();
+
+        public const string Version = "1.0.0";
+        public const string Author = "ryd3v";
+
+        public async Task<int> Start(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Log.Trace($"Starting SCPakTool v{Version}...");
+            Log.Trace($"Created by {Author}.");
+            Log.Write();
+            RootCommand rootCommand = new("A command-line tool for unpacking, repacking, and inspecting Survivalcraft 2 .pak files.");
+            Option<FileInfo> inspectOption = new("--inspect", "-ins")
+            {
+                Description = "Specifies if the program should inspect the .pak file."
+            };
+            rootCommand.Options.Add(inspectOption);
+            rootCommand.SetAction(result => ExecuteCommandAction(result).GetAwaiter().GetResult());
+            var parseResult = rootCommand.Parse(args);
+            return await parseResult.InvokeAsync();
         }
+
+        public async Task<int> ExecuteCommandAction(ParseResult result)
+        {
+            if (result.GetValue<FileInfo>("--inspect") is FileInfo info)
+            {
+                using var fs = info.OpenRead();
+                using var reader = new BinaryReader(fs);
+                string signature = Encoding.UTF8.GetString(reader.ReadBytes(4));
+                if (signature != "PK2\0")
+                {
+                    Log.Error($"Failed to inspect '{info.Name}' because it was not a Survivalcraft 2 .pak file!");
+                    return -1;
+                }
+                //TODO: code to inspect and read shit
+                return 0;
+            }
+            return -1;
+        } 
     }
 }
